f6153a3c6969f8e5017ee9efcf86605adfe92365
Mitch Alforque <malforque717@gmail.com>
2019-01-02 21:12:08 +0800

    added loader

---
+++ app/src/main/java/com/example/administrator/streetfood/AppStartActivity.java
@@ -0,0 +1,19 @@
+package com.example.administrator.streetfood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class AppStartActivity extends AppCompatActivity {
+
+    ImageView appStartLoader;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.app_start);
+
+    }
+}
