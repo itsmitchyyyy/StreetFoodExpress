46998a10e6c0e92ef30a8c155074d7325f1bb750
Mitch Alforque <malforque717@gmail.com>
2019-01-02 21:52:48 +0800

    added register

--- app/src/main/java/com/example/administrator/streetfood/MainActivity.java
+++ app/src/main/java/com/example/administrator/streetfood/MainActivity.java
@@ -4,13 +4,32 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    TextView registerNowButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        registerNowButton = this.findViewById(R.id.textView4);
+
+        registerNowButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch(id){
+            case R.id.textView4:
+                Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
