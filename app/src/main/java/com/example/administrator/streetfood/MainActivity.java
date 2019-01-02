859a30f29bc590286f9eaaae71c98017db24ccbe
Mitch Alforque <malforque717@gmail.com>
2019-01-03 01:54:22 +0800

    firebase

--- app/src/main/java/com/example/administrator/streetfood/MainActivity.java
+++ app/src/main/java/com/example/administrator/streetfood/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-
+    EditText editTxtUsername, editTxtPassword;
+    Button loginButton;
     TextView registerNowButton;
 
     @Override
@@ -18,15 +21,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editTxtUsername = this.findViewById(R.id.editText3);
+        editTxtPassword = this.findViewById(R.id.editText4);
+        loginButton = this.findViewById(R.id.button);
         registerNowButton = this.findViewById(R.id.textView4);
 
         registerNowButton.setOnClickListener(this);
+        loginButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         int id = v.getId();
         switch(id){
+            case R.id.button:
+                break;
             case R.id.textView4:
                 Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(registerIntent);
